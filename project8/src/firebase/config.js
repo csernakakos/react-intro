c1a5a70357081d7b42e40e3b9a9bddfff235841f
csernakakos <csernakakos@gmail.com>
2022-04-19 09:48:02 +0200

    Initialized project8

---
+++ project8/src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCEPJQSGgqmGDMiQ-KPeEtVp2VB0D4MWLs",
+    authDomain: "thedojosite-7fdde.firebaseapp.com",
+    projectId: "thedojosite-7fdde",
+    storageBucket: "thedojosite-7fdde.appspot.com",
+    messagingSenderId: "308801928925",
+    appId: "1:308801928925:web:e0c52d2b29bb872a9be534"
+  };
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// initialize services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp }
